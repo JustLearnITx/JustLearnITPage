bc7bd129aa22a4470aaa35c3cf82842b43df6e8c
oheyek <oheyek@proton.me>
2026-08-22 16:51:34 +0200

    feat(server): add connection to db

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 import express from "express";
+import sqlite3 from "sqlite3";
 
 const app = express();
+const db = new sqlite3.Database("./db/database.db", (error) => {
+  error ? console.error(`Error: ${error}`) : console.log("Connected to db");
+});
+
+db.all(
+  "SELECT id, slug, header, subheader, is_public, created_at, updated_at FROM posts",
+  [],
+  (error, rows) => (error ? console.error(error) : console.log(rows)),
+);
 
 app.use(express.static("./public"));
 
