4286f6651945c9b362a803e1d63aa8663b640047
oheyek <oheyek@proton.me>
2026-08-23 09:53:26 +0200

    feat(server): add courses endpoint

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 import express from "express";
 import sqlite3 from "sqlite3";
+import path from "path";
+
+const __dirname = import.meta.dirname;
 
 const app = express();
 const db = new sqlite3.Database("./db/database.db", (error) => {
-  error ? console.error(`Error: ${error}`) : console.log("Connected to db");
+	error ? console.error(`Error: ${error}`) : console.log("Connected to db");
 });
 
-db.all(
-  "SELECT id, slug, header, subheader, is_public, created_at, updated_at FROM posts",
-  [],
-  (error, rows) => (error ? console.error(error) : console.log(rows)),
-);
+app.get("/courses", (req, res) => {
+	res.sendFile(path.join(__dirname, "public/pages/courses.html"));
+	db.all(
+		"SELECT id, slug, header, subheader, is_public, created_at, updated_at FROM posts",
+		[],
+		(error, rows) => (error ? console.error(error) : console.log(rows)),
+	);
+});
 
 app.use(express.static("./public"));
 
