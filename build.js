b0d992c52ac80d2bc16d44789bf300a407fc8e2a
oheyek <oheyek@proton.me>
2026-09-15 20:50:31 +0200

    feat(build): add hashes to generated dist files

--- build.js
+++ build.js
@@ -1,27 +1,43 @@
-import { readdirSync } from "fs";
+import { readdirSync, writeFileSync, unlinkSync, readFileSync } from "fs";
 import { minify } from "@node-minify/core";
 import { terser } from "@node-minify/terser";
 import { lightningCss } from "@node-minify/lightningcss";
 import { htmlMinifier } from "@node-minify/html-minifier";
+import { createHash } from "crypto";
+import path from "path";
 
 /**
- * Function used to minify all JavaScript files used by static web content.
+ * Function to create hash based on provided content.
+ * @param {string} content - Content to get hash from.
+ * @returns {string} Short MD5 hash (6 characters) calculated from provided content.
+ **/
+const getHash = (content) =>
+	createHash("md5").update(content).digest("hex").slice(0, 6);
+
+/**
+ * Function used to minify all JavaScript files used by static web content with a specific hash.
  **/
 const minifyJS = async () => {
 	const files = readdirSync("public/js");
 	await Promise.all(
-		files.map((file) =>
-			minify({
+		files.map(async (file) => {
+			await minify({
 				compressor: terser,
 				input: `public/js/${file}`,
 				output: `dist/js/${file}`,
-			}),
-		),
+			});
+			const content = readFileSync(`dist/js/${file}`, "utf-8");
+			const hash = getHash(content);
+			const outputFile = `${file.split(".")[0]}.${hash}.js`;
+			const outputPath = path.join("dist/js", outputFile);
+			writeFileSync(outputPath, content);
+			unlinkSync(`dist/js/${file}`);
+		}),
 	);
 };
 
 /**
- * Function used to minify CSS file used in the app.
+ * Function used to minify CSS file used in the app and write that with special hash.
  **/
 const minifyCSS = async () => {
 	await minify({
@@ -29,6 +45,12 @@ const minifyCSS = async () => {
 		input: "public/css/style.css",
 		output: "dist/css/style.css",
 	});
+	const content = readFileSync("dist/css/style.css", "utf-8");
+	const hash = getHash(content);
+	const outputFile = `style.${hash}.css`;
+	const outputPath = path.join("dist/css", outputFile);
+	writeFileSync(outputPath, content);
+	unlinkSync("dist/css/style.css");
 };
 
 /**
